eb2bf4fa42d8c1cfb5cc04f2f31a4b3813d76736
lissms <felissms@gmail.com>
2021-01-26 22:34:55 +0100

    add first styled

--- src/components/MenuItems.jsx
+++ src/components/MenuItems.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
+import { Title, Text, ArrowBtn, TitleContainer, MenuSections } from "./menu.styled";
 
 function MenuItems(props) {
+  const [hasFlag, setHasFlag] = useState(false);
+
+  const handlerFWork = () => {
+    setHasFlag(hasFlag ? false : true);
+  };
   return (
-    <div>
-      <h3>{props.title}</h3>
-      <p>{props.text}</p>
-    </div>
+    <MenuSections>
+      <div className="menu-sections">
+        <TitleContainer>
+          <Title>{props.title}</Title>
+          <ArrowBtn onClick={handlerFWork} hasFlag={hasFlag}>
+            <div className="arrow"></div>
+            <div className="arrow-bottom"></div>
+          </ArrowBtn>
+        </TitleContainer>
+        <Text>{props.text}</Text>
+      </div>
+    </MenuSections>
   );
 }
 
--- src/components/menu.styled.js
+++ src/components/menu.styled.js
@@ -1,1 +1,50 @@
 import styled from "styled-components";
+
+export const MenuSections = styled.div`
+  width: 60%;
+  margin: 20px;
+  & .menu-sections {
+    border-bottom: solid 2px gray;
+  }
+`;
+export const Title = styled.h3`
+  color: blue;
+  padding: 16px;
+`;
+export const Text = styled.p`
+  color: gray;
+  padding: 16px;
+`;
+
+export const TitleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+export const ArrowBtn = styled.div`
+  display: flex;
+  width: 52px;
+  height: 52px;
+  padding: 20px;
+  border-radius: 100px;
+  transform: rotate(180deg);
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  & .arrow {
+    width: 10px;
+    height: 3px;
+    background: blue;
+    transform-origin: left;
+    transform: ${(props) => (props.hasFlag ? `rotate(45deg) translateY(-5px);` : `rotate(-45deg) translateY(5px);`)};
+    transition: transform ease 0.5s;
+  }
+  & .arrow-bottom {
+    width: 10px;
+    height: 3px;
+    background: blue;
+    transform-origin: right;
+    transform: ${(props) => (props.hasFlag ? `rotate(-45deg) translateY(-5px);` : `rotate(45deg) translateY(5px);`)};
+    transition: transform ease 0.5s;
+  }
+`;
